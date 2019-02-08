dccb03674e711ed99303ba27912990a4adb4f100
RM147 <43373305+RM147@users.noreply.github.com>
2019-02-08 09:50:17 +0000

    Add files via upload

---
+++ Recipes2/target/recipeapp2/WEB-INF/classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO RECIPE (id, recipe_name, recipe_owner, description) VALUES (1, 'Toast', 'Gordon Ramsey','Put the f***ing bread in a toaster')
+INSERT INTO RECIPE (id, recipe_name, recipe_owner, description) VALUES (2, 'Roast chicken','Gordon Ramsey','Its f***ing raw!')
+INSERT INTO RECIPE (id, recipe_name, recipe_owner, description) VALUES (3, 'Burnt chicken', 'Rana','Put chicken in oven for seven hours')
+INSERT INTO RECIPE (id, recipe_name, recipe_owner, description) VALUES (4, 'Takeway', 'Lazy Cook','Call a takeaway')
